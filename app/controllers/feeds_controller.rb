bb2209c4e2c8315e17161ab9c4c302cadae2aa94
gijutu <31819030+gijutu@users.noreply.github.com>
2018-08-27 12:03:49 +0900

    HTML・CSS編集

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,20 +1,15 @@
 class FeedsController < ApplicationController
   before_action :set_feed, only: [:show, :edit, :update, :destroy]
 
-  # GET /feeds
-  # GET /feeds.json
   def index
     @feeds = Feed.all
   end
 
-  # GET /feeds/1
-  # GET /feeds/1.json
   def show
     @favorite = current_user.favorites.find_by(feed_id: @feed.id)
     @user = User.find_by(id: @feed.user_id)
   end
 
-  # GET /feeds/new
   def new
     if params[:back]
       @feed = Feed.new(feed_params)
@@ -29,10 +24,8 @@ class FeedsController < ApplicationController
     render :new if @feed.invalid?
   end
 
-  # GET /feeds/1/edit
   def edit; end
-  # POST /feeds
-  # POST /feeds.json
+
   def create
     @feed = Feed.new(feed_params)
     @feed.user_id = current_user.id
@@ -44,8 +37,6 @@ class FeedsController < ApplicationController
       end
   end
 
-  # PATCH/PUT /feeds/1
-  # PATCH/PUT /feeds/1.json
   def update
     @feed.user_id = current_user.id
     if @feed.update(feed_params)
@@ -55,8 +46,6 @@ class FeedsController < ApplicationController
     end
   end
 
-  # DELETE /feeds/1
-  # DELETE /feeds/1.json
   def destroy
     @feed.destroy
     respond_to do |format|
@@ -66,14 +55,14 @@ class FeedsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_feed
       @feed = Feed.find(params[:id])
 
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def feed_params
       params.require(:feed).permit(:image, :image_cache,:content, :user_id)
     end
+
 end
